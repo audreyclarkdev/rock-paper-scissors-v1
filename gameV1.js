cb84faeb64f2663d694b44ea93962903112de2a8
Audrey Clark <37598784+audreyclarkdev@users.noreply.github.com>
2023-12-15 00:45:53 -0600

    Update gameV1.js
    Fixed the code and added a prompt for the user

--- gameV1.js
+++ gameV1.js
@@ -1,4 +1,4 @@
-function getUserChoice(userInput) {
+const getUserChoice = (userInput) => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
         return userInput;
@@ -7,8 +7,8 @@ function getUserChoice(userInput) {
     }
 };
 
-function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){
         return 'rock';
     } else if (randomNumber === 1){
@@ -18,44 +18,54 @@ function getComputerChoice() {
     }
 };
 
-function determineWinner(userChoice, computerChoice) {
+const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === computerChoice) {
         return 'You both tied!';
-    } 
+    }
+
     if (userChoice === 'bomb') {
         return 'You blew up the computer. He was my only friend :(';
     }
+    
     if (userChoice === 'rock') {
         if (computerChoice === 'paper') {
-            return 'The computer beat you!';
-        } else {
+            return 'The computer won!';
+        } 
+        if (computerChoice === 'scissors') {
             return 'Congrats, you win!';
         }
     }
+
     if (userChoice === 'paper') {
         if (computerChoice === 'scissors') {
-            return 'The computer beat you!';
-        } else {
+            return 'The computer won!';
+        } 
+        if (computerChoice === 'rock') {
             return 'Congrats, you win!';
         }
     }
+
     if (userChoice === 'scissors') {
         if (computerChoice === 'rock') {
-            return 'The computer beat you!';
-        } else {
+            return 'The computer won!';
+        } 
+        if (computerChoice === 'paper') {
             return 'Congrats, you win!';
         }
     }
 };
 
-function playGame() {
-    const userChoice = getUserChoice('paper');
+let playGame = (userInput) => {
+    const userChoice = getUserChoice(userInput);
     const computerChoice = getComputerChoice();
 
-    console.log("The user chose", userChoice);   
-    console.log("The computer played", computerChoice);
+    console.log("The User chose", userChoice);   
+    console.log("The Computer chose", computerChoice);
     
+    let winnerText = determineWinner(userChoice, computerChoice);
+    console.log(winnerText);
 };
 
-playGame();
-console.log("And the winner is:", determineWinner('paper', 'rock'));
+let userInput = prompt("Please enter rock, paper, or scissors.");
+
+playGame(userInput);
